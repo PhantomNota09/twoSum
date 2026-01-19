12a4e73c4ebf08a34e4a8a2349aef61b252fcac3
PhantomNota09 <srayala5@asu.edu>
2026-01-19 11:18:01 -0700

    created page where sum of two numbers can be calculated

--- twoSum/TwoSum.swift
+++ twoSum/TwoSum.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TwoSum: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showAnswer.text = ""
     }
-
-
+    
+    
+    @IBOutlet weak var firstNum: UITextField!
+    @IBOutlet weak var secondNum: UITextField!
+    @IBOutlet weak var showAnswer: UILabel!
+    
+    @IBAction func totalSum(_ sender: Any)
+    {
+        guard let text1 = firstNum.text, let num1 = Int(text1),
+              let text2 = secondNum.text, let num2 = Int(text2) else {
+            return
+        }
+        
+        let sum = num1 + num2
+        showAnswer.text = "\(sum)"
+    }
 }
 
